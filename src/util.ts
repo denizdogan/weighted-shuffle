1f33ddc6462ca0069e7b6e5ee90922aa80c8f78b
Benjamin Levesque <14175665+benjlevesque@users.noreply.github.com>
2021-04-21 23:34:10 +0200

    revert type simplification

--- src/util.ts
+++ src/util.ts
@@ -7,7 +7,7 @@ export function weightFunc(weight: number): number {
 
 export function fromArray<T = any>(input: ArrayInput<T>): Tuple<T>[] {
   // apply weightFunc to the second element of every tuple.
-  return input.map(tuple => [tuple[0], weightFunc(tuple[1])])
+  return input.map((tuple: Tuple<T>): Tuple => [tuple[0], weightFunc(tuple[1])])
 }
 
 export function fromObject(input: ObjectInput): Tuple[] {
